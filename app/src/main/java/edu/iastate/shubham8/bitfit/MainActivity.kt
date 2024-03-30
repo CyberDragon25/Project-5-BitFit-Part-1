1c5504e8c90ed678ad2aa0bacf7198ff9f7a859a
Shubham <bhattacharya.shubham123@gmail.com>
2024-03-29 20:30:47 -0500

    created project

---
+++ app/src/main/java/edu/iastate/shubham8/bitfit/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.iastate.shubham8.bitfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
